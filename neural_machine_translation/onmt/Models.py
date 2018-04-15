2621cac6aaf254822f8fd0c0a40bb85d24b270e9
gujiuxiang <gu.jiuxiang@gmail.com>
2018-04-16 02:18:43 +0800

    recover nmt embedding's self.feature_dicts

--- neural_machine_translation/onmt/Models.py
+++ neural_machine_translation/onmt/Models.py
@@ -26,7 +26,22 @@ class Embeddings(nn.Module):
         self.word_lut = nn.Embedding(dicts.size(), opt.word_vec_size, padding_idx=onmt.Constants.PAD)  # Word embeddings.
         self.dropout = nn.Dropout(p=opt.dropout)
         self.feature_dicts = feature_dicts
-        self.feature_luts = nn.ModuleList([])
+        # Feature embeddings.
+        if self.feature_dicts is not None:
+            self.feature_luts = nn.ModuleList([
+                nn.Embedding(feature_dict.size(),
+                             opt.feature_vec_size,
+                             padding_idx=onmt.Constants.PAD)
+                for feature_dict in feature_dicts])
+
+            # MLP on features and words.
+            self.activation = nn.ReLU()
+            self.linear = onmt.modules.BottleLinear(
+                opt.word_vec_size +
+                len(feature_dicts) * opt.feature_vec_size,
+                opt.word_vec_size)
+        else:
+            self.feature_luts = nn.ModuleList([])
 
     def make_positional_encodings(self, dim, max_len):
         pe = torch.FloatTensor(max_len, 1, dim).fill_(0)
@@ -44,15 +59,27 @@ class Embeddings(nn.Module):
     def forward(self, src_input):
         """
         Embed the words or utilize features and MLP.
-        Args: src_input (LongTensor): len x batch x nfeat
-        Return: emb (FloatTensor): len x batch x input_size
+        Args:
+            src_input (LongTensor): len x batch x nfeat
+        Return:
+            emb (FloatTensor): len x batch x input_size
         """
         word = self.word_lut(src_input[:, :, 0])
         emb = word
+        if self.feature_dicts is not None:
+            features = [feature_lut(src_input[:, :, j + 1])
+                        for j, feature_lut in enumerate(self.feature_luts)]
+
+            # Apply one MLP layer.
+            emb = self.activation(
+                self.linear(torch.cat([word] + features, -1)))
+
         if self.positional_encoding:
-            emb = emb + Variable(self.pe[:emb.size(0), :1, :emb.size(2)].expand_as(emb))
+            emb = emb + Variable(self.pe[:emb.size(0), :1, :emb.size(2)]
+                                 .expand_as(emb))
             emb = self.dropout(emb)
         return emb
+
 
 
 class Encoder(nn.Module):
